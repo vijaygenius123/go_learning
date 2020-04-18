a9976d44a03a5021ae2f59ce1dd20f47ec51c7f8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-04-18 23:18:23 +0530

    Parsing Input From CLI

--- consoleInput.go
+++ consoleInput.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
 
 
@@ -18,4 +20,15 @@ func main() {
 	str, _ := reader.ReadString('\n')
 	fmt.Println(str)
 	
+	fmt.Print("Enter a number: ")
+	str,_ = reader.ReadString('\n')
+
+	num, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+
+	if err == nil {
+		fmt.Printf("You entered %v", num)
+	} else{
+		fmt.Printf("Error  %v", err)
+	}
+	 
 }
